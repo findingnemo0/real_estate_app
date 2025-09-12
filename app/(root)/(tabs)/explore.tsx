4655e2771d6d140892a6316779111fa6d1dd4551
Finding_nemo <kamalsingh1472004@gmail.com>
2025-09-13 00:45:38 +0530

    first commit

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const explore = () => {
+  return (
+    <View>
+      <Text>This is explore screen</Text>
+    </View>
+  )
+}
+
+export default explore
---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const profile = () => {
+  return (
+    <View>
+      <Text>This is profile screen</Text>
+    </View>
+  )
+}
+
+export default profile
