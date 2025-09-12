8ad42db6f79103c53ddfa02213a486b171ed54e3
Finding_nemo <kamalsingh1472004@gmail.com>
2025-09-13 01:03:50 +0530

    no change

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -12,7 +12,6 @@ export default function Index() {
     >
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
-      <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
       <Link href="/properties/1">Property</Link>
     </View>
